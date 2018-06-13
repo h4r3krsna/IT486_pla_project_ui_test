a96694228eeaf9ba976a91f0d4c2c8a3c52df3d8
h4r3krsna <manju.sreedaran@gmail.com>
2018-06-13 12:16:37 -0700

    tests for approval form authentication and verification retrieval of dummy submission

--- src/test/java/ApprovalFormTest.java
+++ src/test/java/ApprovalFormTest.java
@@ -2,6 +2,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
 
 public class ApprovalFormTest {
@@ -26,9 +28,31 @@ public class ApprovalFormTest {
   }
 
   @Test
+  public void authenticationChallenge() {
+    WebElement usernameInput = null;
+    try {
+      usernameInput = webDriver.findElement(By.id("username"));
+    } catch (NoSuchElementException exn) {
+      System.err.println(exn.getMessage());
+    } finally {
+      assertNotNull(usernameInput);
+    }
+  }
+
+  @Test
   public void sidVerification(){
-    WebElement element = webDriver.findElement(By.id("student-id"));
-    assertEquals("123456789", element.getAttribute("value"));
+    webDriver.findElement(By.id("username")).sendKeys("admin");
+    WebElement passwordInput = webDriver.findElement(By.id("password"));
+    passwordInput.sendKeys("nimdadrowssap");
+    passwordInput.submit();
+    (new WebDriverWait(webDriver, 10)).until(new ExpectedCondition<Boolean>() {
+      public Boolean apply(WebDriver webDriver) {
+        WebElement element = webDriver.findElement(By.id("student-id"));
+        String expected = "123456789", actual = element.getAttribute("value");
+        assertEquals(expected, actual);
+        return expected.equals(actual);
+      }
+    });
   }
 
   @After
